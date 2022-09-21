73b201ab7b31632891f6ce513d4b2046943028ce
Yasin Adam <74yadam@gmail.com>
2022-09-21 18:04:25 -0400

    Rough draft 1

---
+++ src/Main/LoginMenu.java
@@ -0,0 +1,17 @@
+package Main;
+
+public class LoginMenu {
+
+    public void loginPrompt() {
+
+        System.out.println("Please enter username");
+        String username = Main.sc.nextLine();
+
+        System.out.println("Please enter password");
+        String password = Main.sc.nextLine();
+
+    }
+
+
+
+}
